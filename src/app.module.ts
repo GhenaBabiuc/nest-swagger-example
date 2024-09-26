3dc1a48750f8f9842142f9095f98409bdf91cd68
Ghena <babiuc.ghenadii@gmail.com>
2024-09-26 12:59:07 +0300

    add nest and swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { StudentsModule } from './modules/students/students.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [StudentsModule],
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ src/modules/students/students.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class studentService {
+  private students = [
+    {
+      id: 1,
+      name: 'Elon Musk',
+      mark: 10,
+    }, {
+      id: 2,
+      name: 'Jersey',
+      mark: 9,
+    }, {
+      id: 3,
+      name: 'Milk',
+      mark: 9,
+    }];
+
+  findAll() {
+    return this.students;
+  }
+
+  findOne(id: number) {
+    return this.students.find(item => item.id === id);
+  }
+
+  create(item: any) {
+    this.students.push(item);
+    return item;
+  }
+
+  update(id: number, student: any) {
+    const index = this.students.findIndex(item => item.id === id);
+    if (index > -1) {
+      this.students[index] = { ...this.students[index], ...student };
+      return this.students[index];
+    }
+    return null;
+  }
+
+  remove(id: number) {
+    const index = this.students.findIndex(item => item.id === id);
+    if (index > -1) {
+      return this.students.splice(index, 1);
+    }
+    return null;
+  }
+}
